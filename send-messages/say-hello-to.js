f33cb074b5fc6982d115b41437b645a24ace13b6
Andrew <andrew.lizon12@gmail.com>
2024-04-24 14:03:05 -0700

    Imported say-message into say-hello-to

--- send-messages/say-hello-to.js
+++ send-messages/say-hello-to.js
@@ -1,5 +1,5 @@
-
-
+// The import requires that we use two dots since we are going back to the root directory
+const sayMessage = require('../messages/say-message')
 function sayHelloTo(name) {
   sayMessage(`Hello ${name}!`)
 }
